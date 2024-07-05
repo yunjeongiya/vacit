9a83589a43b970502301e7b3c853920817f09035
twkwon0417 <kwontony123@konkuk.ac.kr>
2024-07-05 22:35:16 +0900

    fix: jpa unalbe to create wallet table

--- src/main/java/com/oursummer/vacit/domain/Wallet.java
+++ src/main/java/com/oursummer/vacit/domain/Wallet.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import java.sql.Timestamp;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
@@ -14,15 +15,18 @@ public class Wallet {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ColumnDefault("0")
     private int coin;
-
-    @ColumnDefault("0")
     private int exp;
     @CreationTimestamp
     private Timestamp createdAt;
     @UpdateTimestamp
     private Timestamp updatedAt;
-    @ColumnDefault("active")
     private String status;
+
+    @PrePersist
+    protected void prePersist() {
+        this.coin = 0;
+        this.exp = 0;
+        this.status = "ACTIVE";
+    }
 }
