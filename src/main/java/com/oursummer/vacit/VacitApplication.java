3d9f4381188be64b012e04f1f9cf57ab9187d9b1
윤정 <70215620+yunjeongiya@users.noreply.github.com>
2024-07-05 14:47:03 +0900

    initial commit

---
+++ src/main/java/com/oursummer/vacit/VacitApplication.java
@@ -0,0 +1,13 @@
+package com.oursummer.vacit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oursummer/vacit/VacitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oursummer.vacit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
