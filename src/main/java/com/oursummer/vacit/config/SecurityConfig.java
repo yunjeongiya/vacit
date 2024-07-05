09b7d1181d90faaabc87e70a7d3bb008aa7fecc1
윤정 <70215620+yunjeongiya@users.noreply.github.com>
2024-07-05 18:01:15 +0900

    feat: oauth response

--- src/main/java/com/oursummer/vacit/config/SecurityConfig.java
+++ src/main/java/com/oursummer/vacit/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.oursummer.vacit.config;
 
+import com.oursummer.vacit.service.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -9,14 +11,20 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf((csrf) -> csrf.disable())
                 .formLogin((login) -> login.disable())
                 .httpBasic((basic) -> basic.disable())
-                .oauth2Login(Customizer.withDefaults())
+                .oauth2Login((oauth2) -> oauth2
+                        .userInfoEndpoint((userInfoEndpointConfig ->
+                                userInfoEndpointConfig.userService(customOAuth2UserService))))
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/", "/oauth2/**", "/login/**").permitAll()
                         .anyRequest().authenticated());
