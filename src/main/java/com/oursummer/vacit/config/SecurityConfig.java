c203f7118c4106a37c09e9dc0c181b21482d91c0
GreenRain <47217640+greenrain78@users.noreply.github.com>
2024-07-06 04:00:34 +0900

    fix: url 권한 수정

--- src/main/java/com/oursummer/vacit/config/SecurityConfig.java
+++ src/main/java/com/oursummer/vacit/config/SecurityConfig.java
@@ -33,7 +33,8 @@ public class SecurityConfig {
                         .requestMatchers("/oauth2/**", "/login/**").permitAll()
                         .requestMatchers("/h2-console/**").permitAll() // h2-console 경로에 대한 접근 허용
                         .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**").permitAll()
-                        //.requestMatchers("/**").permitAll()); // 개발중 모든 사용자에게 허용
+                        .requestMatchers("/stickers", "/stickers/**", "/").permitAll() // 개발중 모든 사용자에게 허용
+//                        .requestMatchers("/**").permitAll() // 개발중 모든 사용자에게 허용
                         .anyRequest().authenticated());
 
         return http.build();
