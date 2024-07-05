441bbe188cb96a5985d30a22df275fd7abc0a43b
GreenRain <47217640+greenrain78@users.noreply.github.com>
2024-07-05 19:51:40 +0900

    feat: stickers 구성 완료

--- src/main/java/com/oursummer/vacit/controller/StickerController.java
+++ src/main/java/com/oursummer/vacit/controller/StickerController.java
@@ -1,27 +1,32 @@
 package com.oursummer.vacit.controller;
 
+import com.oursummer.vacit.domain.Sticker;
+import com.oursummer.vacit.service.StickerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
-@Controller
+@RestController
 public class StickerController {
+
+    private final StickerService stickerService;
 
     @GetMapping("/stickers")
     public ResponseEntity<Object> sticker() {
-
+        log.info("모든 스티커 조회");
         Map<String, String> resp = new HashMap<>();
-        resp.put("name", "sticker");
-        resp.put("description", "sticker description");
-
+        List<Sticker> stickers = stickerService.getSticker();
+        resp.put("message", "모든 스티커 조회");
+        resp.put("data", stickers.toString());
         return ResponseEntity.ok().body(resp);
     }
 
--- src/main/java/com/oursummer/vacit/service/StickerService.java
+++ src/main/java/com/oursummer/vacit/service/StickerService.java
@@ -1,6 +1,22 @@
 package com.oursummer.vacit.service;
 
+import com.oursummer.vacit.domain.Sticker;
+import com.oursummer.vacit.repository.StickerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class StickerService {
+
+    private final StickerRepository stickerRepository;
+
+    public List<Sticker> getSticker() {
+        return stickerRepository.findAll();
+    }
+
+
 
 }
