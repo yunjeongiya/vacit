a53453a982d830cd3dc7e949d25ad36f2881466a
GreenRain <47217640+greenrain78@users.noreply.github.com>
2024-07-06 00:13:12 +0900

    feat: user의 habits 조회

--- src/main/java/com/oursummer/vacit/controller/UserController.java
+++ src/main/java/com/oursummer/vacit/controller/UserController.java
@@ -1,2 +1,28 @@
-package com.oursummer.vacit.controller;public class UserController {
+package com.oursummer.vacit.controller;
+
+import com.oursummer.vacit.domain.Habit;
+import com.oursummer.vacit.dto.user.HabitsResponse;
+import com.oursummer.vacit.service.HabitService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+public class UserController {
+
+    private final HabitService habitService;
+    @GetMapping("/users/{userId}/habbits/")
+    public ResponseEntity<Object> getHabbits(@PathVariable Long userId) {
+        log.info("get user habbits: {}", userId);
+        List<Habit> habits = habitService.getHabitsByUserId(userId);
+        HabitsResponse habitsResponse = new HabitsResponse(habits);
+        return ResponseEntity.ok().body(habitsResponse);
+    }
 }
